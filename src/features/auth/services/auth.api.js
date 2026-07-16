03bc7f1764d1d51ee523c35e974aa3bfe14342d2
Sohel Hussain <sahilmansuri0036@gmail.com>
2026-07-17 02:06:45 +0530

    fix: update backend baseURL for production

--- src/features/auth/services/auth.api.js
+++ src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://gen-ai-backend-z998.onrender.com",
     withCredentials: true
 })
 
